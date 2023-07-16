8a249ffa81003390f5412513dcc28bdf0185fb55
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-07-15 22:13:36 -0300

    teste vercel

--- src/components/Thumb/index.js
+++ src/components/Thumb/index.js
@@ -1,10 +1,11 @@
-//import ThumbImage from '../../assets/ThumbImage.png';
+import ThumbImage from '../../assets/ThumbImage.png'
 import logoWindowns from '../../assets/logoWindows.png';
 import style from './style.module.css';
 
 export default function Thumb() {
     return (
         <div className={style.container} >
+            <img src={ThumbImage} className={style.thumbimage}></img>
             <div className={style.containerbox}>
                 <h1 className={style.titleBox}>ONLINE MATCHES</h1>
                 <p className={style.subtitleBox}>Largest and most trusted top-up websites for games and online entertainment in Asia and beyond.</p>  
